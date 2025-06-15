cc6d4ea50103b6b4c671f07880a4210dc613084e
yantr-manav <saivamshijilla04@gmail.com>
2025-06-16 00:55:10 +0530

    Contains Duplicate

--- Neetcode150/ContainsDuplicate.py
+++ Neetcode150/ContainsDuplicate.py
@@ -1,9 +1,20 @@
 # Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 
-# Solution :
+# Solution 1:
 
 from typing import List
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
+
+
+# Solution 2:
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range (i+1,len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+               
+        return False
